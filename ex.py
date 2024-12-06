959f830727046a6b000125678d9ee6f91ab958cf
gulsahgumuS <gulsahgumuu422@gmail.com>
2024-12-06 20:24:06 +0300

    todo list with alembic

---
+++ ex.py
@@ -0,0 +1,69 @@
+#print("hello,world")
+
+# days= ["monday","thuesday","wed.","tthursday","friday","saturday","sunday"]
+# print(days)
+
+# def name():
+#      return "hello,world"
+# name1=name()
+# print(name1)
+
+
+# def sum(a,b):
+#     return a+b
+
+# def div(a,b):
+#     return a/b
+
+# def mult(a,b):
+#     return a*b
+
+# print(sum(2,3))
+# print(div(10,5))
+# print(mult(5,5))
+
+
+# list1=["monday","thuesday","wednesday","thursday","friday","saturday","sunday"]
+# list2=[]
+
+# for element in list1:
+#     list2.append(element)
+# print(list2)
+
+# class calculator:
+#     def __init__(self):
+#         pass
+
+#     def add(self,a,b):
+#         return int(a)+int(b)
+    
+#     def subs(self,a,b):
+#         return int(a)-int(b)
+    
+#     def mult(self,a,b):
+#         return int(a)*int(b)
+    
+#     def div(self,a,b):
+#         return int(a)/int(b)
+
+#     def num_select(self):
+#         print("You choose a any number a and b:")
+#         num1=input("Enter number a:")
+#         num2=input("Enter number b:")  
+#         return num1,num2
+
+
+# calculator1=calculator()
+# firstnum,secondnum=calculator1.num_select()
+# addition=calculator1.add(firstnum,secondnum)
+# print(addition)
+
+# division=calculator1.div(8,4)
+# print(division)
+
+# multiple=calculator1.mult(5,4)
+# print(multiple)
+
+# subst=calculator1.subs(8,7)
+# print(subst)
+
